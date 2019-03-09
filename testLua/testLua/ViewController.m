6201aefcf00022a869618d0a881c6ce852d797de
[1510360132@qq.com] <1510360132@qq.com>
2019-03-10 01:25:35 +0800

    update

--- testLua/testLua/ViewController.m
+++ testLua/testLua/ViewController.m
@@ -27,10 +27,6 @@
 }
 - (void)testLuacore{
     [self setUp];
-    //    lua_pushinteger (self.state, 10);
-    //    lua_setglobal(self.state, "hehe");
-    //    const NSInteger value = lua_tointeger(self.state, -1);
-    //    NSLog(@"%ld",value);
     //加载lua脚本
     NSString *luaFilePath = [[NSBundle mainBundle] pathForResource:@"file" ofType:@"lua"];
     
@@ -50,14 +46,14 @@
                        lua_tostring(self.state, -1));
             return;
         }
-        /*
+    
          err = lua_pcall(self.state, 0, 0, 0); //表示调用lua函数。其中第二个参数为传入参数的数量，必须与压栈的参数数量一致；第三个参数为返回值的数量，表示调用后其放入栈中的返回值有多少个。
          if (0 != err) { //如果发生了错误，错误信息会放在栈顶
          luaL_error(self.state, "cannot run the lua file: %s",
          lua_tostring(self.state, -1));
          return;
          }
-         */
+         
         NSLog(@"Lua_state initial success");
     }
 }
@@ -68,49 +64,10 @@ int printHelloWorld (lua_State*state){
     return 0;
     
 }
-- (void)testLua0{
-    [self setUp];
-    User *usr = [[User alloc]init];
-    usr.name=@"hehe";
-//    lua_pushlightuserdata(self.state, (__bridge void *)usr);
-//    lua_setglobal(self.state, "userdataVal");
-//
-    void *instanceRef = lua_newuserdata(self.state, sizeof(usr)); //setglobal只是创建了一个lua对象，这个对象不是和oc关联的，它是和oc数据独立的
-    instanceRef = (__bridge_retained void *)usr;
-    lua_setglobal(self.state, "userdataVal");
-   
-    lua_register(self.state, "fuc", printHelloWorld); //注册c方法
-    NSString *luaFilePath = [[NSBundle mainBundle] pathForResource:@"file0" ofType:@"lua"];
-    
-    NSString *luaContent = [NSString stringWithContentsOfFile:luaFilePath
-                            
-                                                     encoding:NSUTF8StringEncoding
-                            
-                                                        error:nil];
-    int err;
-    if (luaContent == nil || [luaContent isEqualToString: @""]) { //判断脚本是否为空
-        NSLog(@"Lua_State initial fail，lua file is nil");
-    }else {
-        err = luaL_loadstring(self.state, [luaContent cStringUsingEncoding: NSUTF8StringEncoding]); //加载lua字符串
-      
-        if (0 != err) { //如果发生了错误，错误信息会放在栈顶
-            luaL_error(self.state, "cannot compile the lua file: %s",
-                       lua_tostring(self.state, -1));
-            return;
-        }
-        err = lua_pcall(self.state, 0, 0, 0); //表示调用lua函数。其中第二个参数为传入参数的数量，必须与压栈的参数数量一致；第三个参数为返回值的数量，表示调用后其放入栈中的返回值有多少个。必须调用这个才能执行lua脚本
-        if (0 != err) { //如果发生了错误，错误信息会放在栈顶
-            luaL_error(self.state, "cannot run the lua file: %s",
-                       lua_tostring(self.state, -1));
-            return;
-        }
-        NSLog(@"Lua_state initial success");
-    }
- 
-}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self testLua0];
+    [self testLua1];
 }
 - (void)testLua1{
     LSCContext *context = [[LSCContext alloc] init];
@@ -122,7 +79,7 @@ int printHelloWorld (lua_State*state){
     //解析lua文件
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *path = [bundle pathForResource:@"file" ofType:@"lua"];
-    [context evalScriptFromFile:path];
+    [context evalScriptFromFile:path]; //LSCExportsTypeManager用来管理供lua调用的oc类，它的instanceMethodRouteHandler方法用来生成oc对象
     
     //获取lua变量
     LSCValue *urlValue = [context getGlobalForName:@"url"];
